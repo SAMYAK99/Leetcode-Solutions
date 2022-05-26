f88f9d1e7b961a1b4d696d8ba649202e646c881a
Samyak Singh <54956876+SAMYAK99@users.noreply.github.com>
2022-05-26 23:55:19 +0530

    Time: 136 ms (25.71%), Space: 48.2 MB (93.35%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int n = nums.size();
+        int reach = 0 ;
+        
+        for(int i=0 ;i<n ;i++){
+            
+            // We can't reached at last as there is no jumps left
+            if(reach < i)
+                return false ; 
+            reach = max(reach , i + nums[i]);
+        }
+        
+        return true ; 
+    }
+};
