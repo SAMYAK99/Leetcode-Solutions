f595e43bb839d785f1de6438b0e6e728d1950b32
Samyak Singh <54956876+SAMYAK99@users.noreply.github.com>
2022-08-04 14:30:40 +0530

    Time: 4 ms (66.43%), Space: 7.6 MB (91.69%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     
-    // Brute Force : Using next permutation 
+    // Simple Backtracking Solution
+    /*
+    Go from index to n – 1 and swap. Once the swap has been done call recursion for the next state.After coming back from the recursion make sure you re-swap it because for the next element the swap will not take place.
+    
+    Time Complexity: O(N! X N)  
+    Space Complexity: O(1)
+    */
+    void calculate(int index ,vector<int>& nums ,vector<vector<int>> &ans  ){
+        
+        // Base case : If we reach at last position n
+        if(index == nums.size()){
+            ans.push_back(nums);
+            return ;
+        }
+        
+        for(int i = index ; i<nums.size() ;i++){
+            swap(nums[index],nums[i]);
+             calculate(index+1 , nums , ans);
+             swap(nums[index],nums[i]);
+        }
+    }
     
     vector<vector<int>> permute(vector<int>& nums) {
         
         vector<vector<int>> ans ; 
-        sort(nums.begin() , nums.end()) ; 
-        int n = nums.size() ; 
+       
+        calculate(0,nums,ans);
         
-    do {
-          ans.push_back(nums) ; 
-       } while (next_permutation(nums.begin(), nums.end()));
-        
-        return ans ; 
+        return ans ;
         
     }
 };
+
