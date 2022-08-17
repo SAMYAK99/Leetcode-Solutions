44f68d30b0138d0a2280370fa33c00d5697143b2
Samyak Singh <54956876+SAMYAK99@users.noreply.github.com>
2022-08-17 10:27:28 +0530

    Time: 355 ms (14.79%), Space: 13.4 MB (60.25%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    /*
+    The basic idea is really simple. We can merge first two lists and then
+    push it back. Keep doing this until there is only one list left in vector.
+    Actually, we can regard this as an iterative divide-and-conquer solution.    
+    */
+    
+    ListNode* merge(ListNode* l1 , ListNode* l2){
+        if(l1 == NULL)
+            return l2;
+        
+        if(l2 == NULL)
+            return l1;
+        
+        if(l1->val <= l2->val){
+            l1->next =  merge(l1->next,l2);
+            return l1;
+        }
+        else{
+            l2->next =  merge(l1,l2->next);
+            return l2;
+        }
+    }
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+         
+         if(lists.size() == 0)
+             return nullptr ; 
+        
+        while(lists.size() > 1){
+            lists.push_back(merge(lists[0],lists[1]));
+            // Erasing first and second lists from Lists
+            lists.erase(lists.begin());
+            lists.erase(lists.begin());
+        }
+        
+        return lists.front();
+    }
+};
