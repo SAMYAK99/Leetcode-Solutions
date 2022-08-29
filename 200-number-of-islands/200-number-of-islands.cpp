f7b93d33686730a029d16eda3d7c8fd0301e9f6b
Samyak Singh <54956876+SAMYAK99@users.noreply.github.com>
2022-08-29 18:09:56 +0530

    Time: 47 ms (69.24%), Space: 12.3 MB (55.87%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
-        if( n == 0) return 0;
-        int island = 0;
+        if( n == 0)
+            return 0;
         
+        int island = 0;   
+       
         for( int i =0 ; i< n ; i++){
              for(int j = 0 ; j< grid[0].size() ; j++){
+                // Mark an complete Island in one DFS Call 
                  if(grid[i][j] == '1') {
                     dfs(grid , i , j);
                     island++;
@@ -17,14 +20,21 @@ public:
     }
         
         void dfs(vector<vector<char>>& grid , int i , int j ){
-            if( i< 0 || j < 0 || i> grid.size()-1 || j> grid[0].size() -1) return;
             
-            if(grid[i][j] == '0') return ;
+            // Base Case
+            if( i < 0 || j < 0 || i> grid.size()-1 || j > grid[0].size() -1 || grid[i][j] == '0')
+                return;
+          
             
             grid[i][j] = '0';
+            
             dfs(grid , i+1 , j);
             dfs(grid , i , j+1);
             dfs(grid , i-1 , j);
             dfs(grid , i , j-1);
         }
 };
+
+/*
+-> Space complexity : 0(m*n)
+*/
