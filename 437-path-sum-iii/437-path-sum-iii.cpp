d70669bef7734e5aa84f2a2ab1d099bb9aeab2a5
Samyak Singh <54956876+SAMYAK99@users.noreply.github.com>
2022-09-14 10:50:47 +0530

    Time: 43 ms (42.77%), Space: 19 MB (44.90%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -16,10 +16,12 @@ public:
     Brute Force : O(n^2)
     Fix a node and move towards all the node in the tree
 
-    
+    Optimezed : Use a hashmap to store all freq with their count sum
     */
     
     long long total = 0 ; 
+    map<long,long> mp ;
+    
     
     int pathSum(TreeNode* root, int targetSum) {
         
@@ -27,8 +29,7 @@ public:
             return total;
         
         findPathSum(root,targetSum , 0);
-        pathSum(root->left , targetSum);
-        pathSum(root->right ,targetSum);
+      
         
         return total ;
         
@@ -39,13 +40,25 @@ public:
         if(!root)
             return ;
         
+         //Path sum from root
         currentSum += root->val ; 
+        
         
         if(currentSum == targetSum)
             total++;
         
-        findPathSum(root->left , targetSum , currentSum);
-        findPathSum(root->right ,targetSum , currentSum) ;
+         //checking whether any target sum path present in the path from 
+        // root to the current node
+        if(mp.find(currentSum - targetSum) != mp.end())
+            total +=   mp[currentSum - targetSum];
         
+       mp[currentSum]++;
+        
+        findPathSum(root->left , targetSum , currentSum);
+        findPathSum(root->right ,targetSum , currentSum);
+        
+        // After visiting the left and right subtree, we have to reduce this
+        // path sum count from map since we are leaving this path
+          mp[currentSum]--;
     }
 };
