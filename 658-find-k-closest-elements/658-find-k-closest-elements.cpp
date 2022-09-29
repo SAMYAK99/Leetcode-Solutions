b8e5db674190d816a37ee5af400553215dc021f5
Samyak Singh <54956876+SAMYAK99@users.noreply.github.com>
2022-09-29 19:31:46 +0530

    Time: 91 ms (61.54%), Space: 31.2 MB (86.01%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -5,28 +5,16 @@ public:
     Brute Force : Use max Heap
     2 Pointer apprach
     */
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+    vector<int> findClosestElements(vector<int>& A ,  int k, int x) {
         
-          vector<int>  ans ;
-         
-        int i=0 , j = arr.size()-1;
-        
-        /*
-        Closest elements will be consecutive
-        */
-        
-        while(j-i >= k){
-            if(x-arr[i] <= arr[j] - x)
-                j--;
+           int left = 0, right = A.size() - k;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - A[mid] > A[mid + k] - x)
+                left = mid + 1;
             else
-                i++;
+                right = mid;
         }
-        
-        for( ; i<=j ; i++){
-            ans.push_back(arr[i]);
-        }
-        
-        return ans ;
-       
+        return vector<int>(A.begin() + left, A.begin() + left + k);
     }
 };
