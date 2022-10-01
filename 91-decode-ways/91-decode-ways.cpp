bc10f04d275169961a67bf35c45bc89f5f5cdab6
Samyak Singh <54956876+SAMYAK99@users.noreply.github.com>
2022-10-01 19:39:18 +0530

    Time: 3 ms (63.51%), Space: 7.4 MB (22.23%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -2,33 +2,55 @@ class Solution {
 public:
     
     // Classical DP Problem 
+    /*
+     at every index in the string we have got 2 choices :
+
+Pick only current element as a single value between [1-9]
+Pick 2 elements i.e current as well as next element to make a couple. This value will be [10, 26]
+    
+    */
+    
+    int dp[102];
+    
+    int decode(string s , int idx , int n){
+        
+        // base case 
+        /*
+        If current element is 0, we simply return 0 as it is not possible to get
+        a character using 0. Zero will only be used in couple with some number 
+        occuring before it. Ex : 1 0 , or 2 0 
+        */
+        
+        if(idx < n and s[idx] == '0')
+            return 0 ;
+        
+        if(idx >= n)
+            return 1 ;
+        
+        // Memoziation
+        if(dp[idx] != -1)
+            return dp[idx] ;
+        
+        // Storing final answer
+        int ways = 0 ; 
+        
+        // Picking single
+      ways = decode(s, idx+1, n);
+        
+       // Pick couple : Checking ranges of 1 to 19 + 20 - 26
+        if(idx+1 < n && ((s[idx] == '1' && s[idx+1] <= '9') || (s[idx]=='2' && s[idx+1] < '7')))
+           ways += decode(s, idx+2, n);
+           
+        return dp[idx] = ways; 
+        
+    }
     
     int numDecodings(string s) {
+     
+         int n = s.size();
+        memset(dp, -1, sizeof(dp));
         
-       vector<int> dp(s.size()+1);
-        dp[0]=1;
-        
-        if(s[0]=='0')
-            dp[1]=0;
-        else 
-            dp[1]=1;
-        
-        for(int i=2;i<=s.size();i++){
-            int way1,way2;
-            if(s[i-1]=='0')
-                way1 = 0;
-            else 
-                way1 = dp[i-1];
-            
-            // In the range between 0 to 26
-            if(stoi(s.substr(i-2,2))<=26 && stoi(s.substr(i-2,2))>0 
-               && s[i-2]!='0')
-                way2 = dp[i-2];
-            else 
-                way2 = 0;
-            
-            dp[i] = way1 + way2;
-        }
-        return dp[s.size()];
+         return decode(s , 0 , n) ;
+      
     }
 };
